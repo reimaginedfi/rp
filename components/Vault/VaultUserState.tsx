7fef4207a0397e173f8ea71acda0e5510242feaa
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2022-07-06 02:23:13 +0700

    decimals

--- components/Vault/VaultUserState.tsx
+++ components/Vault/VaultUserState.tsx
@@ -1,3 +1,4 @@
+import { BigNumber } from "ethers";
 import { formatUnits } from "ethers/lib/utils";
 import { useAccount, useContractRead, useToken } from "wagmi";
 import { ContractConfig } from "../../contracts";
@@ -33,12 +34,11 @@ export const VaultUserState = ({
       <AsciiText padStart={2}>
         has pending deposit: {hasPendingDeposit.data ? "true" : "false"}
       </AsciiText>
-      <AsciiText padStart={2}>pending deposit: 0.0 {symbol}</AsciiText>
 
-      <AsciiText padStart={2} opacity={0.5}>
-        // total deposited:{" "}
+      <AsciiText padStart={2}>
+        total deposited:{" "}
         {formatUnits(
-          (user.data?.[0] ?? 0) + (sharesValue.data ?? 0),
+          BigNumber.from(user.data?.[0] ?? 0).add(sharesValue.data ?? 0),
           assetToken.data?.decimals ?? 0
         )}{" "}
         {symbol}
