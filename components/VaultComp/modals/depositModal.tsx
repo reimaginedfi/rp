790658d8ea4c93e8681784c64db9578c64089041
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-07-15 17:31:08 +0530

    Add Withdraw modal

--- components/VaultComp/modals/depositModal.tsx
+++ components/VaultComp/modals/depositModal.tsx
@@ -1,81 +1,82 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 import {
-    ModalOverlay,
-    ModalCloseButton,
-    ModalContent,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    Heading,
-    Input,
-    Button,
-    Grid,
-    Text,
-    useColorMode
-  } from "@chakra-ui/react";
-  //Wagmi
-  import { useNetwork } from 'wagmi';
+  ModalOverlay,
+  ModalCloseButton,
+  ModalContent,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Heading,
+  Input,
+  Button,
+  Grid,
+  Text,
+  useColorMode,
+  Flex,
+  Stack,
+} from "@chakra-ui/react";
+//Wagmi
+import { useNetwork } from "wagmi";
 
-  //Vaults
-  import {useVaultDeposit} from "../../hooks/useVault"
-  import {vaults} from "../../../contracts"
+//Vaults
+import { useVaultDeposit } from "../../hooks/useVault";
+import { vaults } from "../../../contracts";
 
-  type ModalProps = {
-    onClose?: () => void;
-    isOpen?: boolean;
-  };
+type ModalProps = {
+  onClose?: () => void;
+  isOpen?: boolean;
+};
 
-  export default function DepositModal({isOpen, onClose}: ModalProps) {
-    const {colorMode} = useColorMode();
-    const {chain} = useNetwork();
+export default function DepositModal({ isOpen, onClose }: ModalProps) {
+  const { colorMode } = useColorMode();
+  const { chain } = useNetwork();
 
-    const [contractConfig, setContractConfig] = useState<any>();
-    const [amount, setAmount] = useState<string>();
+  const [contractConfig, setContractConfig] = useState<any>();
+  const [amount, setAmount] = useState<string>();
 
-    useEffect(() => {
-      vaults[chain!.id].map((contract) => setContractConfig(contract));
-    }, [vaults])
+  useEffect(() => {
+    vaults[chain!.id].map((contract) => setContractConfig(contract));
+  }, [vaults]);
 
-    // const {    balance,
-    //     balanceDisplay,
-    //     allowance,
-    //     isAllowed,
-    //     approve,
-    //     isApproving,
-    //     approveMax,
-    //     isApprovingMax,
-    //     storeAsset,
-    //     isStoring} = useVaultDeposit(contractConfig!, amount!);
+  // const {    balance,
+  //     balanceDisplay,
+  //     allowance,
+  //     isAllowed,
+  //     approve,
+  //     isApproving,
+  //     approveMax,
+  //     isApprovingMax,
+  //     storeAsset,
+  //     isStoring} = useVaultDeposit(contractConfig!, amount!);
 
-    return (
-        <Modal
-        isCentered
-        scrollBehavior="inside"
-        size="md"
-        onClose={onClose!}
-        isOpen={isOpen!}>
-          <ModalOverlay onClick={onClose} />
-          <ModalContent>
-            <ModalHeader>
-              <Heading variant="large" textAlign="center">
-                DEPOSIT
-              </Heading>
-            </ModalHeader>
-            <ModalCloseButton _focus={{ boxShadow: "none" }} />
-    
-            <ModalBody
-              px="0.25rem"
-              borderTop="solid 1px"
-              borderColor={colorMode === "dark" ? "#232323" : "#F3F3F3"}
-            >
-                <Input 
-                    onChange={(e) => setAmount(e.target.value)}
-                    value={amount}
-                />
-                <Button variant="primary">Deposit {amount!}</Button>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-    )
-  }
+  return (
+    <Modal
+      isCentered
+      scrollBehavior="inside"
+      size="md"
+      onClose={onClose!}
+      isOpen={isOpen!}
+    >
+      <ModalOverlay onClick={onClose} />
+      <ModalContent>
+        <ModalHeader>
+          <Heading variant="large" textAlign="center">
+            DEPOSIT
+          </Heading>
+        </ModalHeader>
+        <ModalCloseButton _focus={{ boxShadow: "none" }} />
+
+        <ModalBody
+          px="0.25rem"
+          borderTop="solid 1px"
+          borderColor={colorMode === "dark" ? "#232323" : "#F3F3F3"}
+        >
+          
+
+          <Button variant="primary">Deposit {amount!}</Button>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
--- components/VaultComp/modals/withdrawModal.tsx
+++ components/VaultComp/modals/withdrawModal.tsx
@@ -1,52 +1,82 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 import {
-    ModalOverlay,
-    ModalCloseButton,
-    ModalContent,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    Heading,
-    Button,
-    Grid,
-    Text,
-    useColorMode
-  } from "@chakra-ui/react";
+  ModalOverlay,
+  ModalCloseButton,
+  ModalContent,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Heading,
+  Button,
+  Grid,
+  Text,
+  useColorMode,
+  Stack,
+  Flex,
+  Input,
+  Container,
+} from "@chakra-ui/react";
 
-  type ModalProps = {
-    onClose?: () => void;
-    isOpen?: boolean;
-  };
+type ModalProps = {
+  onClose?: () => void;
+  isOpen?: boolean;
+};
 
-  export default function DepositModal({isOpen, onClose}: ModalProps) {
-    const {colorMode} = useColorMode();
-    const [amount, setAmount] = useState<string>();
+export default function WithdrawModal({ isOpen, onClose }: ModalProps) {
+  const { colorMode } = useColorMode();
+  const [amount, setAmount] = useState<string>();
 
-    return (
-        <Modal
-        isCentered
-        scrollBehavior="inside"
-        size="md"
-        onClose={onClose!}
-        isOpen={isOpen!}>
-          <ModalOverlay onClick={onClose} />
-          <ModalContent>
-            <ModalHeader>
-              <Heading variant="large" textAlign="center">
-                Withdraw
-              </Heading>
-            </ModalHeader>
-            <ModalCloseButton _focus={{ boxShadow: "none" }} />
-    
-            <ModalBody
-              px="0.25rem"
-              borderTop="solid 1px"
-              borderColor={colorMode === "dark" ? "#232323" : "#F3F3F3"}
-            >
-                                <Button variant="primary">Unlock {amount!}</Button>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-    )
-  }
+  return (
+    <Modal
+      isCentered
+      scrollBehavior="inside"
+      size="md"
+      onClose={onClose!}
+      isOpen={isOpen!}
+    >
+      <ModalOverlay onClick={onClose} />
+      <ModalContent>
+        <ModalHeader>
+          <Heading variant="large" textAlign="center">
+            Withdraw
+          </Heading>
+        </ModalHeader>
+        <ModalCloseButton _focus={{ boxShadow: "none" }} />
+
+        <ModalBody
+          px="0.25rem"
+          borderTop="solid 1px"
+          borderColor={colorMode === "dark" ? "#232323" : "#F3F3F3"}
+        >
+          <Container>
+            <Stack gap={2} mx={2} my={3}>
+              <Text variant="large">Your Vault Tokens</Text>
+              <Flex alignItems="center" gap={6}>
+                <Flex
+                  fontSize={{ base: "1rem", md: "2rem" }}
+                  alignItems="center"
+                  gap={3}
+                >
+                  <Input
+                    fontWeight={600}
+                    type="number"
+                    w={{ base: "5rem", sm: "10rem" }}
+                    onChange={(e) => setAmount(e.target.value)}
+                    value={amount}
+                  />{" "}
+                  <Text fontWeight={600}>VT</Text>
+                </Flex>
+                <Text fontWeight={600}>500 USDC</Text>
+              </Flex>
+              <Text variant="large">Max 10.00 VT</Text>
+            </Stack>
+            <Button mt={"4rem"} w="80%" m="auto" variant="primary">
+              Unlock {amount!}
+            </Button>
+          </Container>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
