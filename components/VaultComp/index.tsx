217be29aa6b73500a4956688e52b0d43fab901bd
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2022-07-22 04:27:57 +0700

    vault details

--- components/VaultComp/index.tsx
+++ components/VaultComp/index.tsx
@@ -17,6 +17,7 @@ import {
   Image,
   useDisclosure,
   Link,
+  SkeletonText,
 } from "@chakra-ui/react";
 
 import VaultProgressBar from "./VaultProgressBar";
@@ -26,6 +27,7 @@ import UserStat from "../UserStat";
 import { ContractConfig } from "../../contracts";
 import { commify } from "ethers/lib/utils";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { useContractRead } from "wagmi";
 
 type VaultProps = {
   vaultName: string;
@@ -57,6 +59,18 @@ const VaultComp = ({
     onOpen: onOpenWithdraw,
     onClose: onCloseWithdraw,
   } = useDisclosure();
+
+  const farmer = useContractRead({
+    ...contractConfig,
+    functionName: "farmer",
+    watch: true,
+  });
+
+  const feeReceiver = useContractRead({
+    ...contractConfig,
+    functionName: "feeDistributor",
+    watch: true,
+  });
 
   return (
     <>
@@ -105,9 +119,9 @@ const VaultComp = ({
               ) : (
                 <GridItem textAlign="center">
                   <Text fontSize="32px" fontWeight={600}>
-                    0%
+                    +0%
                   </Text>
-                  <Text fontSize="24px">0 {asset}</Text>
+                  <Text fontSize="24px">+0 {asset}</Text>
                   <Text
                     style={{
                       backgroundClip: "text",
@@ -176,23 +190,6 @@ const VaultComp = ({
               <Text variant="medium">{pendingDeposit} USDC</Text>
             </Flex>
 
-            <Flex justifyContent={"center"} alignItems={"center"} mb={0}>
-              <Link
-                size={"xs"}
-                fontSize={"xs"}
-                textAlign={"center"}
-                verticalAlign={"center"}
-                href={`https://etherscan.io/address/${contractConfig.addressOrName}`}
-                isExternal
-              >
-                {`${contractConfig.addressOrName.slice(
-                  0,
-                  6
-                )}...${contractConfig.addressOrName.slice(-4)}`}{" "}
-                <ExternalLinkIcon mx="2px" />
-              </Link>
-            </Flex>
-
             <Accordion borderRadius="1rem" mt="1rem" allowToggle border="none">
               <AccordionItem border="none">
                 <AccordionButton
@@ -200,7 +197,7 @@ const VaultComp = ({
                   justifyItems="space-between"
                   justifyContent="space-between"
                 >
-                  <Heading variant="medium">Show Your Vault Stats</Heading>
+                  <Heading variant="medium">Your Vault Stats</Heading>
                   <AccordionIcon />
                 </AccordionButton>
                 <AccordionPanel
@@ -208,6 +205,100 @@ const VaultComp = ({
                   bg={colorMode === "dark" ? "#1C1C1C" : "#F8F8F8"}
                 >
                   <UserStat contractConfig={contractConfig} />
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+            <Accordion borderRadius="1rem" mt="1rem" allowToggle border="none">
+              <AccordionItem border="none">
+                <AccordionButton
+                  borderRadius="1rem"
+                  justifyItems="space-between"
+                  justifyContent="space-between"
+                >
+                  <Heading variant="medium">Vault Details</Heading>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel
+                  borderRadius="1rem"
+                  bg={colorMode === "dark" ? "#1C1C1C" : "#F8F8F8"}
+                >
+                  <Flex alignItems={"center"}>
+                    <Text variant="medium">Contract Address</Text>
+                    <Spacer />
+                    {farmer.isLoading ? (
+                      <SkeletonText />
+                    ) : (
+                      <Link
+                        href={`https://etherscan.io/address/${contractConfig.addressOrName}`}
+                        isExternal
+                      >
+                        <Text variant="medium">
+                          {`${contractConfig.addressOrName.slice(
+                            0,
+                            6
+                          )}...${contractConfig.addressOrName.slice(-4)}`}{" "}
+                          <ExternalLinkIcon mx="2px" />
+                        </Text>
+                      </Link>
+                    )}
+                  </Flex>
+                  <Flex alignItems={"center"}>
+                    <Text variant="medium">Farmer Address</Text>
+                    <Spacer />
+                    {farmer.isLoading ? (
+                      <SkeletonText />
+                    ) : (
+                      <Link
+                        href={`https://etherscan.io/address/${farmer.data}`}
+                        isExternal
+                      >
+                        <Text variant="medium">
+                          {`${farmer.data?.slice(0, 6)}...${farmer.data?.slice(
+                            -4
+                          )}`}{" "}
+                          <ExternalLinkIcon mx="2px" />
+                        </Text>
+                      </Link>
+                    )}
+                  </Flex>
+                  <Flex alignItems={"center"}>
+                    <Text variant="medium">Fee Receiver</Text>
+                    <Spacer />
+                    {feeReceiver.isLoading ? (
+                      <SkeletonText />
+                    ) : (
+                      <Link
+                        href={`https://etherscan.io/address/${feeReceiver.data}`}
+                        isExternal
+                      >
+                        <Text variant="medium">
+                          {`${feeReceiver.data?.slice(
+                            0,
+                            6
+                          )}...${feeReceiver.data?.slice(-4)}`}{" "}
+                          <ExternalLinkIcon mx="2px" />
+                        </Text>
+                      </Link>
+                    )}
+                  </Flex>
+                  <Flex alignItems={"center"}>
+                    <Text variant="medium">Entry/Exit Fee</Text>
+                    <Spacer />
+                    {feeReceiver.isLoading ? (
+                      <SkeletonText />
+                    ) : (
+                      <Text variant="medium">1%</Text>
+                    )}
+                  </Flex>
+                  <Flex alignItems={"center"}>
+                    <Text variant="medium">Management Fee</Text>
+                    <Spacer />
+                    {feeReceiver.isLoading ? (
+                      <SkeletonText />
+                    ) : (
+                      <Text variant="medium">20%</Text>
+                    )}
+                  </Flex>
                 </AccordionPanel>
               </AccordionItem>
             </Accordion>
