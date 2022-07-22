dcac88a58be1f86545bd6d2012eb8401e2ca5de0
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2022-07-22 10:19:40 +0700

    dryrun and token

--- next.config.js
+++ next.config.js
@@ -20,6 +20,8 @@ const sentryWebpackPluginOptions = {
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
+  dryRun: process.env.VERCEL_ENV !== "production",
+  authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN,
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
