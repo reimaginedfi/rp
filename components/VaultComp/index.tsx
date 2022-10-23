fd317710d77defc3c728a0b432be6f338c7f15df
Angel <angel333gomez@gmail.com>
2022-10-23 15:39:04 -0300

    fixed blocknumber

--- components/VaultComp/index.tsx
+++ components/VaultComp/index.tsx
@@ -135,11 +135,9 @@ const VaultComp = ({
     ).toNumber();
   
     // CURRENT CHAIN BLOCK - to calculate against management block
-    // const blockNumber = useBlockNumber({
-    //   watch: true,
-    // });
-
-    // console.log(blockNumber)
+    const blockNumber = useBlockNumber({
+      watch: true,
+    });
 
   return (
     <>
@@ -181,9 +179,9 @@ const VaultComp = ({
 
                   </GridItem>
 
-                  {/* {lastManagementBlock > (blockNumber.data ?? 0) ? ( */}
-                    {/* <></>
-                  ) : ( */}
+                  {lastManagementBlock > (blockNumber.data ?? 0) ? (
+                     <></>
+                  ) : ( 
                     <>
                     <GridItem alignItems="center">
                      <DepositButton
@@ -199,7 +197,7 @@ const VaultComp = ({
                      </Button>
                    </GridItem>
                     </>
-                  {/* )} */}
+                  )}
                 </Grid>
                 {+pendingDeposit + +currentAum > 0 &&
                   (+pendingDeposit + +currentAum) / +aumCap > 0.95 &&
