34e48ed9e5323ca0f41ac38b2676ab54e8c88183
Angel <angel333gomez@gmail.com>
2022-11-23 12:28:58 -0300

    withdraw copy 2

--- components/VaultComp/modals/withdrawModal.tsx
+++ components/VaultComp/modals/withdrawModal.tsx
@@ -251,7 +251,7 @@ export default function WithdrawModal({ isOpen, onClose }: ModalProps) {
           </Stack>
         ) : needToClaim ? (
           <Stack alignContent="center">
-            <Text mb="1rem" textAlign="center"> You have unclaimed vault tokens (VT). You need to claim these VT tokens first. Then you can unlock and eventually withdraw them.</Text>
+            <Text mb="1rem" textAlign="center"> You have unclaimed vault tokens (VT). You need to claim these VT tokens first. Then you can unlock and eventually withdraw them (they convert into USDC).</Text>
             <Button
             size={"sm"}
             colorScheme="orange"
